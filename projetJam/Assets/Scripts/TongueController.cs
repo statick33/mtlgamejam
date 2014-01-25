2365a84f88bed93795fe08dd24e8e633e24da93b
statick33 <alexgilbert33@hotmail.com>
2014-01-25 14:20:18 -0500

    Added tongue controller

---
+++ projetJam/Assets/Scripts/TongueController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TongueController : MonoBehaviour
+{
+
+    private bool isCurrentlyShooting;
+
+    Pawn pawn;
+
+    void Start()
+    {
+        pawn = transform.parent.parent.GetComponent<Pawn>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space") && isCurrentlyShooting == false)
+        {
+            isCurrentlyShooting = true;
+            InvokeRepeating("ShootTongue", 0, 0.01f);
+            pawn.SetLockAction(true, false);
+        }
+    }
+    void ShootTongue()
+    {
+       // print("SHOOT");
+        Vector3 movement = new Vector3(0, 0, 0.05f);
+        transform.position = transform.position + movement;
+    }
+
+    void RetractTongue()
+    {
+      //  print("retract");
+        Vector3 movement = new Vector3(0, 0, -0.05f);
+        transform.position = transform.position + movement;
+    }
+
+    void OnCollisionEnter(Collision target)
+    {
+        if (target.gameObject.tag == "Bounce")
+        {
+            CancelInvoke("ShootTongue");
+            InvokeRepeating("RetractTongue", 0, 0.01f);
+
+        }
+        else if (target.gameObject.tag == "Player")
+        {
+            Vector3 vect = new Vector3(0, 0, 0);
+            CancelInvoke("RetractTongue");
+            rigidbody.velocity = vect;
+
+            vect.z = 1;
+            vect = vect + target.transform.position;
+            rigidbody.position = vect;
+            isCurrentlyShooting = false;
+        }
+        else
+        {
+            print(target.gameObject.tag);
+        }
+
+    }
+}
